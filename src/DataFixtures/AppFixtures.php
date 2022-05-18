ca37c859bc6f2d2fcdf54b6997ad42d728109664
Anthony <a@a.a>
2022-05-18 16:29:40 +0200

    Ajout des fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+
+    /**
+     * Stockage du service d'encodage des mot de passe de Symfony
+     */
+    private $encoder;
+    private $slugger;
+
+    /**
+     * On utilise le constructeur pour demander à Symfony de récupérer le service d'encodage des mot de passes, pour ensuite le stocker dans $this->>encoder
+     */
+    public function __construct(UserPasswordHasherInterface $encoder, SluggerInterface $slugger){
+        $this->encoder = $encoder;
+        $this->slugger = $slugger;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        // Instanciation du Faker en langue fr
+        $faker = Faker\Factory::create('fr_FR');
+
+
+        // Création du compte admin de Batman
+        $admin = new User();
+
+        $admin
+            ->setEmail('admin@a.a')
+            ->setRegistrationDate( $faker->dateTimeBetween('-1 year', 'now') )
+            ->setPseudonym('Batman')
+            ->setRoles( ["ROLE_ADMIN"] )
+            ->setPassword(
+                $this->encoder->hashPassword($admin, 'aaaaaaaaA7/')
+            )
+        ;
+
+        $manager->persist($admin);
+
+        // Création de 10 comptes utilisateur (avec une boucle)
+        for($i = 0; $i < 10; $i++){
+
+            $user = new User();
+
+            $user
+                ->setEmail( $faker->email )
+                ->setRegistrationDate( $faker->dateTimeBetween('-1 year', 'now') )
+                ->setPseudonym( $faker->userName )
+                ->setPassword(
+                    $this->encoder->hashPassword($user, 'aaaaaaaaA7/')
+                )
+            ;
+
+            $manager->persist($user);
+
+        }
+
+        // Création de 200 articles (avec une boucle)
+        for($i = 0; $i < 200; $i++){
+
+            $article = new Article();
+
+            $article
+                ->setTitle( $faker->sentence(10) )
+                ->setContent( $faker->paragraph(15) )
+                ->setPublicationDate( $faker->dateTimeBetween('-1 year', 'now') )
+                ->setAuthor( $admin )   // Batman sera l'auteur de tous les articles
+                ->setSlug( $this->slugger->slug( $article->getTitle() )->lower() )
+            ;
+
+            $manager->persist( $article );
+
+        }
+
+
+        $manager->flush();
+    }
+}
