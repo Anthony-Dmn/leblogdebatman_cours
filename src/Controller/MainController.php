21eaccfd49a9f9d087828bfd195d159c2235a8aa
Anthony <a@a.a>
2022-05-19 09:59:41 +0200

    Ajout derniers articles sur l'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,9 +18,21 @@ class MainController extends AbstractController
      * Contrôleur de la page d'accueil
      */
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
-        return $this->render('main/home.html.twig');
+
+        // Récupération des derniers articles à afficher sur l'accueil
+        $articleRepo = $doctrine->getRepository(Article::class);
+
+        $articles = $articleRepo->findBy(
+            [],     // WHERE du SELECT
+            ['publicationDate' => 'DESC'],      // ORDER BY du SELECT
+            $this->getParameter('app.article.last_article_number_on_home')  // LIMIT du SELECT (qu'on récupère dans services.yaml)
+        );
+
+        return $this->render('main/home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
     /**
